74c310472f0dc5fc83c72c25e5ca77c583c999a3
Rajendra Kumar R Yadav <rajendra.ecti@gmail.com>
2021-09-14 15:55:46 +0900

    Step 6 completed!

--- custom_order.java
+++ custom_order.java
@@ -57,6 +57,9 @@ public class custom_order {
         // TEST CODE
 
         // STEP 6 PROMPT USER TO CHOOSE FROSTING
+        System.out.println("What type of FROSTING do you want? ");
+        System.out.println("Vanilla, Chocolate, Strawberry or Coco");
+        frostingType = scanner.nextLine();
 
 
         //TEST CODE
